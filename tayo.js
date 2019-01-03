4ef29a4d07a93dfdb6b211ae2cc0441ef080dc2f
Cesara Rinda Ahadirohman <32482447+cesararinda@users.noreply.github.com>
2019-01-03 23:16:42 +0700

    Add files via upload

---
+++ tayo.js
@@ -0,0 +1,9 @@
+var y;
+    var bidag=new Array();
+    bidang[0]="Umum";
+    bidang[1]="Administrasi";
+    bidang[2]="Bimingan Konseling"
+
+    for(x in bidang)
+    {
+        document.write(bidang[y] + "<br />")
